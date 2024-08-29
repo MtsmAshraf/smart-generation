185593ee3c3fac36bc9b89656335b77b7515e43c
moatasim <mo32000a@gmail.com>
2024-08-29 08:06:36 +0300

    added the other routes (no contetn yet) and edited the services cards style

--- src/app/(subpages)/services/page.tsx
+++ src/app/(subpages)/services/page.tsx
@@ -1,8 +1,15 @@
+import Contact from '@/components/contact/contact'
+import ServicesCards from '@/components/servicesCards/servicesCards'
 import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section>
+        <div className="container">
+            <ServicesCards></ServicesCards>
+            <Contact></Contact>
+        </div>
+    </section>
   )
 }
 
