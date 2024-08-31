44611ff7e88d8d45f2e7efb857144bd3251ffac3
moatasim <mo32000a@gmail.com>
2024-08-31 07:40:25 +0300

    added some style to the  slider (sliding functionality not added yet)

--- src/components/projectsSlider/projectsSlider.tsx
+++ src/components/projectsSlider/projectsSlider.tsx
@@ -5,34 +5,46 @@ import test from "../../assets/imgs/1.jpg"
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import MainHeading from '../mainHeading/mainHeading'
+import Link from 'next/link'
 const ProjectsSlider = () => {
     const projectsIds = ["1","2","3","4","5","6","7","8","9"]
     return (
-    <div className={styles.projectsSlider}>
-        <div className={styles.arrow + " " + styles.right}>
-            <FontAwesomeIcon icon={faChevronRight} />
+        <section className={styles.section}>
+            <MainHeading>
+                <h2>جميع أعمالنا</h2>
+            </MainHeading>
+            <div className={styles.projectsSlider}>
+                <button className={styles.arrow + " " + styles.right}>
+                    <FontAwesomeIcon icon={faChevronRight} />
+                </button>
+                <button className={styles.arrow + " " + styles.left}>
+                    <FontAwesomeIcon icon={faChevronLeft} />
+                </button>
+                <div>
+                    {projectsIds.map((id) => {
+                        return(
+                            <Link href={`projects/${id}`}>
+                                <ProjectCard>
+                                    <div className={styles.cardImg}>
+                                        <Image src={test} alt='Project Card'></Image>
+                                    </div>
+                                    <div className={styles.cardText}>
+                                        <h2>
+                                            مشروع {id}
+                                        </h2>
+                                        <span>
+                                            مصر
+                                        </span>
+                                    </div>
+                                </ProjectCard>
+                            </Link>
+                        )
+                    })}
+                </div>
             </div>
-        <div className={styles.arrow + " " + styles.left}>
-            <FontAwesomeIcon icon={faChevronLeft} />
-        </div>
-        <div>
-            {projectsIds.map((id) => {
-                return(
-                    <ProjectCard>
-                        <div className={styles.cardImg}>
-                            <Image src={test} alt='Project Card'></Image>
-                        </div>
-                        <h2>
-                            مشروع {id}
-                        </h2>
-                        <span>
-                            مصر
-                        </span>
-                    </ProjectCard>
-                )
-            })}
-        </div>
-    </div>
+        </section>
+    
   )
 }
 
