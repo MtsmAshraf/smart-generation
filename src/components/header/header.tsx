54af28c7cba498f4a0984e611315bc7eb216aad6
moatasim <mo32000a@gmail.com>
2024-10-02 08:06:45 +0300

    added products imgs and added the products links to the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,14 +8,16 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Button from '../button/button'
 import { usePathname } from 'next/navigation'
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faChevronDown, faChevronRight, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const Header = () => {
     const pathname = usePathname()
+    const [isClient, setIsClient] = useState(false)
     const [scrolled, setScrolled] = useState(false)
     useEffect(() => {
+        setIsClient(true)
         window.onscroll = () => {
             if(window.scrollY >= 200){
                 setScrolled(true)
@@ -68,7 +70,117 @@ const Header = () => {
                     المشاريع
                 </Link>
                 <Link className={pathname === "/supplies" ? styles.active : ""} href={"/supplies"}>
-                    التوريدات
+                    <span>
+                        المنتجات
+                    </span>
+                    <FontAwesomeIcon icon={faChevronDown}></FontAwesomeIcon>
+                    {
+                        isClient ? 
+                        <div className={styles.moreLinks}>
+                        <ul>
+                            <li>
+                                <Link href={"/"}>
+                                    <span>
+                                        Inverter
+                                    </span>
+                                    <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+                                    <ul>
+                                        <li>
+                                            <Link href={"/"}>
+                                                VTS Series Universal Variable Speed AC Drive
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link href={"/"}>
+                                                VTS30 Series frequency converter
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link href={"/"}>
+                                                V9 Series High Performance Universal Speed AC Drive
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/"}>
+                                    <span>
+                                        PLC
+                                    </span>
+                                    <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+                                    <ul>
+                                        <li>
+                                            <Link href={"/"}>
+                                                VC10V Simple and sporty small PLC
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link href={"/"}>
+                                                VC20 Series high-performance general-purpose PLC
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/"}>
+                                    <span>
+                                        HMI
+                                    </span>
+                                    <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+                                    <ul>
+                                        <li>
+                                            <Link href={"/"}>
+                                                VT DP-2000 series HMI
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/"}>
+                                    <span>
+                                        Servo Drive
+                                    </span>
+                                    <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+                                    <ul>
+                                        <li>
+                                            <Link href={"/"}>
+                                                VS510 Series servo drive
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link href={"/"}>
+                                                VS512 series servo driver
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/"}>
+                                    <span>
+                                        Accessories
+                                    </span>
+                                    <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+                                    <ul>
+                                        <li>
+                                            <Link href={"/"}>VTS30</Link>
+                                        </li>
+                                        <li>
+                                            <Link href={"/"}>VTS30</Link>
+                                        </li>
+                                        <li>
+                                            <Link href={"/"}>VTS30</Link>
+                                        </li>
+                                    </ul>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                        : null 
+                    }
                 </Link>
                 <Link className={pathname === "/about" ? styles.active : ""} href={"/about"}>
                     عن الشركة
